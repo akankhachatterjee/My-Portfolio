6d61541a915ac2d5b4082ee3440df0c560663e6f
Akankha <49246594+akankhachatterjee@users.noreply.github.com>
2020-09-05 22:05:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+document.querySelector('.hobby').addEventListener('click',hobbies);
+document.querySelector('.skill').addEventListener('click',skills);
+document.querySelector('.education').addEventListener('click',education);
+
+
+function hobbies()
+{
+    alert('Hi!!3 of my fav hobbies are Travelling,Reading and Dancing');
+}
+
+function skills(){
+    alert('My Technical Skills are Java,Spring and Web Development');
+}
+
+function education(){
+    alert('I have completed by B.Tech from Techno India,Kolkata');
+}
